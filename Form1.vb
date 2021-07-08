5c3600cddc6cc270f8b009132ca87ba5a5c41a1c
batch-engineering <geoftay@gmail.com>
2021-07-08 14:42:03 +0800

    first commit

---
+++ Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Label1.Text = "Ko" Then
+            Label1.Text = "Ok"
+        Else
+            Label1.Text = "Ko"
+        End if
+
+    End Sub
+End Class
